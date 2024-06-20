8dc8c56dadab476df12f7baa3fcedf86e075aae0
narek <narek@DESKTOP-98JS365>
2024-06-20 10:45:39 +0300

    Добавьте файлы проекта.

---
+++ 3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string inputString = "Пример строки";
+        int length = CalculateStringLength(inputString);
+        Console.WriteLine($"Длина строки \"{inputString}\" равна {length}");
+    }
+
+    static int CalculateStringLength(string input)
+    {
+        return input.Length;
+    }
+}
+
